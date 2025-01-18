bc69c99da489a1a4db1e4dad0aab896c08a3eaf1
CommitWiz <commitwiz@outlook.com>
2025-01-18 13:03:21 +0530

    rectify vercel timeout

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -2,21 +2,22 @@ import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
+export const maxDuration =30;
 export async function POST(request: NextRequest) {
-    const session = await getServerSession(authOptions);
-    if(!session){
-        return NextResponse.json({error :"unauthorized" },{status:401})
-    }
-    const{prompt} = await request.json();
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+  }
+  const { prompt } = await request.json();
 
-    function generateRandomNumber(): number{
-        return Math.floor(Math.random()*100000000)+1;
+  function generateRandomNumber(): number {
+    return Math.floor(Math.random() * 100000000) + 1;
+  }
+  const randomSeed = generateRandomNumber();
+  const imageUrl = `https://image.pollinations.ai/prompt/${encodeURIComponent(
+    prompt
+  )}?seed=${randomSeed}&width=512&height=512&noLogo=True`;
 
-    }
-    const randomSeed =generateRandomNumber();
-    const imageUrl=`https://image.pollinations.ai/prompt/${encodeURIComponent(prompt)}?seed=${randomSeed}&width=512&height=512&noLogo=True`;
-
-    await fetch (imageUrl);
-    return NextResponse.json({url:imageUrl});
+  await fetch(imageUrl);
+  return NextResponse.json({ url: imageUrl });
 }
-
