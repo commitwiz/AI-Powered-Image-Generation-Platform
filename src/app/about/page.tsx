6a7ba5736fa6dae47642f14d4f3fbe3315bad15d
CommitWiz <commitwiz@outlook.com>
2025-02-03 21:52:15 +0530

    chore: comment out unused Image import in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Github, Mail, Globe } from 'lucide-react'
 import Link from 'next/link'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
